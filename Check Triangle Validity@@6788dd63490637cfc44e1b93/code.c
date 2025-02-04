d47906f5c1a1d653f83b192c19ab6e55ef270595
puranjay369 <puranjay369@gmail.com>
2025-02-04 18:43:38 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a*a+b*b>c*c && b*b+c*c>a*a && a*a+c*c>b*b){
+        printf("Valid");
+    }
+    else{
+        printf("Invalid");
+    }
+    
+
+    return 0;
+}
