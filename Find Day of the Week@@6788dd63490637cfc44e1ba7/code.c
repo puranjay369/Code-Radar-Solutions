bc9770c242bfce5648120642bee1c98a86e08b53
puranjay369 <puranjay369@gmail.com>
2025-02-06 16:47:27 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -3,34 +3,31 @@
 
     int main(){
 
-        //switch= a more effective alternative to using many "else if" statements
-        //        allows a value to be tested for equality against many cases
-
         int grade;
 
         scanf("%d",&grade);
 
         switch (grade)
         {
-        case 1:
+        case '1':
             printf("Monday");
             break;
-        case 2:
+        case '2':
             printf("Tuesday");
             break;
-        case 3:
+        case '3':
             printf("Wednesday");
             break;
-        case 4:
+        case '4':
             printf("Thursday");
             break;        
-        case 5:
+        case '5':
             printf("Friday");
             break;
-        case 6:
+        case '6':
             printf("Saturday");
             break;
-         case 7:
+         case '7':
             printf("Sunday");
             break;    
         default:
